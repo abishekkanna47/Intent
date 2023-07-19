759f11bf24d8ae885908fc5cee398d0f4cb9ceaa
AK <chroniclesofak47@gmail.com>
2023-07-19 15:38:23 +0530

    .

---
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.intent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button calb = findViewById(R.id.cal);
+        calb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_APP_CALENDAR);
+
+                startActivity(intent);
+            }
+        });
+
+        Button calcb = findViewById(R.id.calc);
+        calcb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_APP_CALCULATOR);
+
+                startActivity(intent);
+            }
+        });
+
+        Button brb = findViewById(R.id.br);
+        brb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+}
